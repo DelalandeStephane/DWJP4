b46477cb072b39204250c8cb56386461bbeacc99
StephaneDe <delalandestephane@hotmail.fr>
2019-01-18 17:20:07 +0100

    Ajout: Structure HTML de la page template

---
+++ view/frontend/template.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title><?= $title ?></title>
+	<link rel="stylesheet" type="text/css" href="../../public/css/style.css"><!-- A changer lors du passage à index.php -->
+</head>
+<body>
+	<div>
+		<header>
+			<h1><a href="#">Billet simple pour l'Alaska</a></h1>
+			<ul>
+				<li><a href="*">Accueil</a></li>
+				<li><a href="*">L'auteur</a></li>
+				<li><a href="*">Administration</a></li>
+				<li><a href="*">Nouveau chapitre</a></li>
+			</ul>
+		</header>
+		<section><!-- Content -->
+			<article>
+				<h3>Chapitre 1</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+			</article>
+			<article>
+				<h3>Chapitre 2</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+			</article>
+		</section><!-- Content -->
+	</div>
+	<footer>
+		<p>Delalande Stephane -Dans le cadre du projet 4  DWJ -OpenClass room-</p>
+		<ul>
+			
+		</ul><!-- Reseaux sociaux -->
+	</footer>
+</body>
+</html>
