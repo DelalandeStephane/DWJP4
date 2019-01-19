adf29b5257df005194be8f737c7eb6952e75fffe
StephaneDe <delalandestephane@hotmail.fr>
2019-01-19 16:49:02 +0100

    Ajout: Mise en place du css pour la page d'accueil

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -2,58 +2,55 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title><?= $title ?></title>
+	<title></title>
 	<link rel="stylesheet" type="text/css" href="../../public/css/style.css"><!-- A changer lors du passage à index.php -->
 </head>
 <body>
-	<div>
+	<div class="container">
 		<header>
 			<h1><a href="#">Billet simple pour l'Alaska</a></h1>
 			<ul>
 				<li><a href="*">Accueil</a></li>
 				<li><a href="*">L'auteur</a></li>
-				<li><a href="*">Administration</a></li>
 				<li><a href="*">Nouveau chapitre</a></li>
+				<li><a href="*">Administration</a></li>
 			</ul>
 		</header>
-		<section><!-- Content -->
-			<article>
+			<div id="nav-bar-index-page" >
+				<div class="triangle nav-bar-page" id="triangle-left">
+				</div>
+				<div class="triangle nav-bar-page" id="triangle-right">
+				</div>	
+			</div>
+		<section id="index-content"><!-- Content -->
+			<article class="index-page">
 				<h3>Chapitre 1</h3>
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.
 				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua.
 			</p>
+			<a href="#">Lire plus</a>
 			</article>
-			<article>
+			<article class="index-page" >
 				<h3>Chapitre 2</h3>
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.
 				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua.
 			</p>
+			<a href="#">Lire plus</a>
 			</article>
 		</section><!-- Content -->
 	</div>
 	<footer>
 		<p>Delalande Stephane -Dans le cadre du projet 4  DWJ -OpenClass room-</p>
-		<ul>
-			
-		</ul><!-- Reseaux sociaux -->
 	</footer>
 </body>
 </html>
