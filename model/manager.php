5a1c9d4fb3487e2b74608087a1a6e1e73b800fac
StephaneDe <delalandestephane@hotmail.fr>
2019-01-19 17:35:39 +0100

    Ajout: Connection base de donnée

---
+++ model/manager.php
@@ -0,0 +1,11 @@
+<?php
+namespace delalande\forteroche\model ;
+
+class Manager {
+	protected function dbConnect ()
+		{
+	        $db = new \PDO('mysql:host=localhost;dbname=book1;charset=utf8', 'root', '');
+	        return $db;
+		}
+		
+}
