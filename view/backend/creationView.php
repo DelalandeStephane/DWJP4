0e016147024077d2f45bf3722119e2e7c7aa53b8
StephaneDe <delalandestephane@hotmail.fr>
2019-01-23 19:47:02 +0100

    Ajout : creation du template backend et vue creation d article

---
+++ view/backend/creationView.php
@@ -0,0 +1,19 @@
+<?php ob_start() ?>
+<form>
+	<textarea id="write-area"></textarea>
+	<input type="submit" value="Envoyer" class="form-bt backend-bt">
+</form>
+
+
+   
+ <script src='http://cloud.tinymce.com/5-testing/tinymce.min.js'></script>
+ <script>
+  tinymce.init({
+    selector: '#write-area',
+    height:800,
+    resize:false
+  });
+  </script>
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php');?>
---
+++ view/backend/template.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="../../public/css/style.css">
+	<link rel="stylesheet" type="text/css" href="../../public/css/backend.css">
+</head>
+<body>
+	<div class="container container-backend">
+		<nav>
+			<div class="backend-nav-deco-square"></div>
+			<ul>
+				<li><a href="#">Création d'article</a></li>
+				<li><a href="#">Liste des articles</a></li>
+				<li><a href="#">Vos commentaires</a></li>
+				<li><a href="#">Commentaires signalés</a></li>
+			</ul>
+		</nav>
+		<section class="backend-content">
+			<?= $content ?>
+		</section>
+	</div>
+	<footer>
+		<p>Delalande Stephane -Dans le cadre du projet 4  DWJ -OpenClass room-</p>
+	</footer>
+</body>
+</html>
